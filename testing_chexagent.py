d13456609930b6a125cd6be49df5d9384bb94247
mahfuzalhasan <mahfuzalhasan058@gmail.com>
2024-12-10 20:40:34 -0500

    loading images

--- testing_chexagent.py
+++ testing_chexagent.py
@@ -15,8 +15,9 @@ generation_config = GenerationConfig.from_pretrained("StanfordAIMI/CheXagent-8b"
 model = AutoModelForCausalLM.from_pretrained("StanfordAIMI/CheXagent-8b", torch_dtype=dtype, trust_remote_code=True)
 
 # step 3: Fetch the images
-image_path = "https://upload.wikimedia.org/wikipedia/commons/3/3b/Pleural_effusion-Metastatic_breast_carcinoma_Case_166_%285477628658%29.jpg"
-images = [Image.open(io.BytesIO(requests.get(image_path).content)).convert("RGB")]
+image_path = "pleural_effusion.jpg"
+images = [Image.open(image_path).convert("RGB")]
+print(f'type:{type(images)}')
 
 # step 4: Generate the Findings section
 prompt = f'Describe "Airway"'
